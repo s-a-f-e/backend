5a07654b0b8a1157d93f46b768b0af550e2623e7
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-05-02 13:55:06 -0700

    starts REST

---
+++ people/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+# from uuid import uuid4
+# from phonenumber_field.modelfields import PhoneNumberField
+# from phonenumber_field.phonenumber import PhoneNumber
+
+from django.contrib.auth.models import User
+
+
+class Person(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=50, blank=False)
+    phone = models.CharField(max_length=20, blank=False)
+    # phone = PhoneNumberField(null=False, blank=False, unique=True)
+    # phone = PhoneNumber.from_string(phone_number=raw_phone, region='RU').as_e164
+
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+
+    # when added
+    created_at = models.DateTimeField(auto_now_add=True)
+    # when added or modified
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+class Driver(Person):
+    homebase = models.CharField(max_length=100, blank=False)
+    available = models.BooleanField()
+
+
+class Mother(Person):
+    village = models.CharField(max_length=50, blank=False)
--- safe/settings.py
+++ safe/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'people',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
