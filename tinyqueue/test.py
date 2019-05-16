7820af0736a9ba468c3e4d4c4e463ca67da53384
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-05-16 10:27:45 -0700

    updates tests

--- tinyqueue/test.py
+++ tinyqueue/test.py
@@ -1,11 +1,31 @@
 import random
 from tinyqueue import TinyQueue
-# test data
 
+# test
 data = []
 for i in range(0, 50):
     data.append(random.randint(0, 101))
 
 sorted = sorted(data)
-print(data)
-print(sorted)
+
+print("maintains priority queue")
+queue = TinyQueue()
+for i in range(0, len(data)):
+    queue.push(data[i])
+
+print(queue.peek() == sorted[0])
+
+result = []
+while queue.length > 0:
+    result.append(queue.pop())
+
+print(result == sorted)
+
+print("accepts data in constructor")
+queue = TinyQueue(data)
+
+result = []
+while (queue.length > 0):
+    result.append(queue.pop())
+
+print(result == sorted)
