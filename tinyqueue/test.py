0b27fc9ec9459587b15fdb59cdb4f6f13cb9fa31
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-05-16 10:33:20 -0700

    finishes tests- all passing

--- tinyqueue/test.py
+++ tinyqueue/test.py
@@ -13,13 +13,19 @@ queue = TinyQueue()
 for i in range(0, len(data)):
     queue.push(data[i])
 
-print(queue.peek() == sorted[0])
+if queue.peek() == sorted[0]:
+    print("PASSED")
+else:
+    print("FAILED")
 
 result = []
 while queue.length > 0:
     result.append(queue.pop())
 
-print(result == sorted)
+if result == sorted:
+    print("PASSED")
+else:
+    print("FAILED")
 
 print("accepts data in constructor")
 queue = TinyQueue(data)
@@ -28,4 +34,31 @@ result = []
 while (queue.length > 0):
     result.append(queue.pop())
 
-print(result == sorted)
+if result == sorted:
+    print("PASSED")
+else:
+    print("FAILED")
+
+
+print('handles edge cases with few elements')
+queue = TinyQueue()
+
+queue.push(2)
+queue.push(1)
+queue.pop()
+queue.pop()
+queue.pop()
+queue.push(2)
+queue.push(1)
+if queue.pop() == 1 and queue.pop() == 2 and queue.pop() == None:
+    print("PASSED")
+else:
+    print("FAILED")
+
+print('handles init with empty array')
+queue = TinyQueue([])
+
+if queue.data == []:
+    print("PASSED")
+else:
+    print("FAILED")
