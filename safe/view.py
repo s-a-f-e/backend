109bced8a62aebb96edc54169f1a9c674fcc8d88
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-06-06 15:41:26 -0700

    adds __init_ so can user modules adds look-up mother by phone number endpoint

--- safe/view.py
+++ safe/view.py
@@ -1,6 +1,5 @@
-from people.models import Village
+from people.models import Village, Mother, Driver
 from django.http import JsonResponse, Http404
-
 
 def village(request, id):
     try:
@@ -12,4 +11,22 @@ def village(request, id):
         }
     except Village.DoesNotExist:
         raise Http404("Village does not exist")
+    return JsonResponse(data)
+
+
+def mother(request, id):
+    try:
+        v_obj = Mother.objects.get(phone=id)
+        data = {
+            'name': v_obj.name,
+            'phone': v_obj.phone,
+            'village': v_obj.village,
+            'latitude': v_obj.latitude,
+            'longitude': v_obj.longitude,
+        }
+        # drivers =  Driver.objects.values()
+        # for d in drivers:
+
+    except Mother.DoesNotExist:
+        raise Http404("Mother does not exist")
     return JsonResponse(data)
