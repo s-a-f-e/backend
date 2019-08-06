a342907339bdde3838ec26b7ef2f551170a31f95
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-08-06 13:22:11 -0700

    adds ability to export village and healthcenter entries as CSV

--- people/admin.py
+++ people/admin.py
@@ -26,10 +26,52 @@ class PersonAdmin(admin.ModelAdmin):
 
     export_as_csv.short_description = "Export Selected"
 
+class VillageAdmin(admin.ModelAdmin):
+    actions = ["export_as_csv"]
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(
+            meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field)
+                                   for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+class HealthCenterAdmin(admin.ModelAdmin):
+    actions = ["export_as_csv"]
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(
+            meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field)
+                                   for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
 
 # Register your models here.
 admin.site.register(Mother, PersonAdmin)
 admin.site.register(Driver, PersonAdmin)
 admin.site.register(Midwife, PersonAdmin)
-admin.site.register(Village)
-admin.site.register(HealthCenter)
+admin.site.register(Village, VillageAdmin)
+admin.site.register(HealthCenter, HealthCenterAdmin)
